cdf53fe107b528b9755920d4271ccf887519c353
Haoyan.Li <haoyan.li.ac@gmail.com>
2023-01-20 19:11:12 +0900

    wip

---
+++ renderer/src/media.ts
@@ -0,0 +1,147 @@
+import * as mediasoup from "mediasoup-client"
+import { io, Socket } from 'socket.io-client'
+
+const api: any = (window as any).api
+
+const handlerName = mediasoup.detectDevice()
+
+if (handlerName) {
+  console.info("detect handler: %s", handlerName)
+}
+else {
+  console.warn("no suitable device")
+}
+
+let socket: Socket
+let device: mediasoup.types.Device
+
+export function create() {
+  socket = io("http://jp.virbea.com:8080") 
+  device = new mediasoup.Device()
+
+  socket.on('connect', () => {
+    console.log('connect to server.')
+  })
+  
+  socket.on("connect_error", (err) => {
+      console.log(`connect_error due to ${err.message}`);
+  });
+
+  start()
+}
+
+
+function sendMsRequest(request: any, cb: (response: any) => void) {
+  socket.emit('ms-request', request, cb)
+}
+
+async function getRouterRtpCapabilites() {
+  return new Promise<mediasoup.types.RtpCapabilities>(
+    resolve => {
+      sendMsRequest(
+        {
+          method: 'getRouterRtpCapabilities',
+        },
+        (response) => {
+          console.info(response)
+
+          resolve(response.payload as mediasoup.types.RtpCapabilities)
+        })
+    })
+ 
+}
+
+async function getTransportOptions() {
+  return new Promise<mediasoup.types.TransportOptions>(
+    resolve => {
+      sendMsRequest(
+        {
+          method: "getTransportOptions",
+        },
+        (response) => {
+          console.info(response)
+
+          resolve(response.payload)
+        }
+      )
+    }
+  )
+}
+
+interface TransportInfo {
+  id: string,
+  iceParameters: mediasoup.types.IceParameters,
+  iceCandidates: mediasoup.types.IceCandidate,
+  dtlsParameters: mediasoup.types.DtlsParameters,
+  sctpParameters: mediasoup.types.SctpParameters
+}
+
+async function connectTransport(dtlsParameters: mediasoup.types.DtlsParameters) {
+  return new Promise<void>(
+    resolve => {
+      sendMsRequest(
+        {
+          method: 'connectProducer',
+          payload: dtlsParameters
+        },
+        (response) => {
+          console.info(response)
+
+          resolve()
+        }
+      )
+    }
+  )
+}
+
+async function createProducer(parameters: {
+  kind: mediasoup.types.MediaKind;
+  rtpParameters: mediasoup.types.RtpParameters;
+  appData: Record<string, unknown>;
+}) {
+  return new Promise<void>(
+    resolve => {
+      sendMsRequest(
+        {
+          method: 'createProducer',
+          payload: parameters
+        },
+        (response) => {
+          console.info(response)
+
+          resolve()
+        }
+      )
+    }
+  )
+}
+
+let transport: mediasoup.types.Transport
+
+async function start() {
+  var rtp = await getRouterRtpCapabilites()
+
+  await device.load({ routerRtpCapabilities: rtp })
+
+  var options = await getTransportOptions()
+
+  transport = device.createSendTransport(options)
+
+  transport.on('connect', async ({dtlsParameters}) => {
+    // call transport.connect() on server
+    await connectTransport(dtlsParameters)
+  })
+
+  transport.on('produce', async (parameters) => {
+    // create producer on server
+    await createProducer(parameters)
+  })
+
+  if (device.canProduce("video")) {
+    var stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
+
+    await transport.produce({track: stream.getVideoTracks()[0]})
+  }
+
+}
+
